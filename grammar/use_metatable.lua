43a89750d2f6114c2d1f6c714ed4dfdcf2e89304
sunfeilong <sunfeilong1993@gmail.com>
2018-11-23 00:11:36 +0800

    代理模式

--- grammar/use_metatable.lua
+++ grammar/use_metatable.lua
@@ -1,29 +1,34 @@
-local Triangle = {}
+local index = {}
 
-Triangle.mt = { a = 3, b = 4, c = 5 }
-
-Triangle.mt.default = 0
-
-Triangle.mt.__index = function(t, k)
-    print("access:" .. tostring(k))
-    if t[k] then
-        return t[k]
-    else
-        return Triangle.mt.default
+local mt = {
+    -- __index 当表里面不存在对应的k时会调用
+    __index = function(t, k)
+        print("access:" .. tostring(k))
+        return t[index][k]
+    end,
+    -- __newindex 设置值的时候如果存在 __newindex 函数则会调用该函数而不会执行赋值操作
+    __newindex = function(t, k, v)
+        print(string.format("set value, key:%s, value: %s", tostring(k), tostring(v)))
+        t[index][k] = v
     end
+}
+
+-- 代理模式
+
+function track(t)
+    local proxy = {}
+    proxy[index] = t
+    setmetatable(proxy, mt)
+    return proxy
 end
 
-Triangle.mt.__newindex = function(t, k, v)
-    print(string.format("set value, key:%s, value: %s", tostring(k), tostring(v)))
+local t = { a = 1, b = 2, c = 3, d = 4 }
+
+_t = track(t)
+
+print(_t["a"])
+
+_t["e"] = 5
+for k, v in pairs(t) do
+    print(k)
 end
-
-local triangle = {}
-
-setmetatable(triangle, Triangle.mt)
-
-print(triangle["a"])
-print(triangle["b"])
-print(triangle["c"])
-print(triangle["d"])
-triangle["d"] = 3
-print(triangle["d"])
